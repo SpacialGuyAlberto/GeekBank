5fbdbcf9b927d2d743ee5abfc453750abdfb9f39
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2024-09-18 18:46:18 +0200

    Commit #24: Agregados los datos de environment

---
+++ src/main/java/com/geekbank/bank/config/ConfigLoader.java
@@ -0,0 +1,16 @@
+package com.geekbank.bank.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConfigLoader {
+
+    private static final Dotenv dotenv = Dotenv.configure()
+            .directory("./")
+            .ignoreIfMissing()
+            .load();
+
+    public static String getVariable(String key) {
+        return dotenv.get(key);
+    }
+
+}
