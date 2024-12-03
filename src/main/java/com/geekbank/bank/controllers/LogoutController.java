78f7c82983cc91129ba76cfbdce6c0c5709e0770
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2024-12-03 19:36:16 +0100

    correccion de carrito

--- src/main/java/com/geekbank/bank/controllers/LogoutController.java
+++ src/main/java/com/geekbank/bank/controllers/LogoutController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/basic")
 public class LogoutController {
 
     private final JwtBlacklistService jwtBlacklistService;
@@ -30,17 +30,17 @@ public class LogoutController {
 //            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid token");
 //        }
 //    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletResponse response) {
-        Cookie jwtCookie = new Cookie("jwtToken", null);
-        jwtCookie.setHttpOnly(true);
-        jwtCookie.setSecure(false);
-        jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(0); // Eliminar la cookie
-        response.addCookie(jwtCookie);
-        return ResponseEntity.ok().build();
-    }
+//
+//    @PostMapping("/logout")
+//    public ResponseEntity<Void> logout(HttpServletResponse response) {
+//        Cookie jwtCookie = new Cookie("jwtToken", null);
+//        jwtCookie.setHttpOnly(true);
+//        jwtCookie.setSecure(false);
+//        jwtCookie.setPath("/");
+//        jwtCookie.setMaxAge(0); // Eliminar la cookie
+//        response.addCookie(jwtCookie);
+//        return ResponseEntity.ok().build();
+//    }
 
 
 }
