d5fbb669b97846bcac17af64fbde1ee49a5b058e
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2025-01-14 13:13:24 +0100

    Correccion en el pago

--- src/main/java/com/geekbank/bank/controllers/MainScreenGiftCardController.java
+++ src/main/java/com/geekbank/bank/controllers/MainScreenGiftCardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,10 +29,12 @@ public class MainScreenGiftCardController {
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+        // Forzamos, por ejemplo, orden asc por ID
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id"));
         Page<MainScreenGiftCardItemDTO> items = mainScreenGiftCardService.getMainScreenGiftCardItems(pageable);
         return ResponseEntity.ok(items);
     }
+
 
     @PostMapping
     public ResponseEntity<List<MainScreenGiftCardItem>> addMainScreenGiftCardItems(@RequestBody MainScreenGiftCardRequest request) {
